5be932a4e098abb95d649caf00ab5081ebe64342
TimGeyssens <tg@umbraco.com>
2015-09-21 13:39:07 +0200

    Updates minor version

--- PageNotFoundManager/Properties/VersionInfo.cs
+++ PageNotFoundManager/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.927.36624")]
+[assembly: AssemblyFileVersion("1.0.927.37221")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
 
 
