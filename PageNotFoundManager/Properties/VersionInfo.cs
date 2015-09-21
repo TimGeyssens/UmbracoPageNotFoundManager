d2e0c6728b795c08835e47d97fc79ab252347af8
TimGeyssens <tg@umbraco.com>
2015-09-21 14:00:37 +0200

    Adds appveyor.yml

--- PageNotFoundManager/Properties/VersionInfo.cs
+++ PageNotFoundManager/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.927.37289")]
+[assembly: AssemblyFileVersion("1.1.927.38210")]
 [assembly: AssemblyInformationalVersion("1.1.0")]
 
 
