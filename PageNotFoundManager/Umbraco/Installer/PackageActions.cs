f7a7f866f43b1ce4c1b28707237d5ce5b56dfa2f
Jeavon <jeavon@crumpled-dog.com>
2015-10-13 11:31:17 +0100

    Adding uninstall support for PNFM.TransformConfig package action even though it's not needed on this package it's useful for others! Note the change to the xdtfile parameter in package.xml, it now doesn't include the ending .install.xdt (or uninstall.xdt)

--- PageNotFoundManager/Umbraco/Installer/PackageActions.cs
+++ PageNotFoundManager/Umbraco/Installer/PackageActions.cs
@@ -10,26 +10,31 @@ namespace PageNotFoundManager.Umbraco.Installer
 {
     public class PackageActions
     {
-        public class TransformConfig: IPackageAction
+        public class TransformConfig : IPackageAction
         {
-
             public string Alias()
             {
                 return "PNFM.TransformConfig";
             }
 
-            public bool Execute(string packageName, System.Xml.XmlNode xmlData)
+            private bool Transform(string packageName, System.Xml.XmlNode xmlData, bool uninstall = false)
             {
-                
-
                 //The config file we want to modify
                 var file = xmlData.Attributes.GetNamedItem("file").Value;
+
                 string sourceDocFileName = VirtualPathUtility.ToAbsolute(file);
-                
+
                 //The xdt file used for tranformation 
-                var xdtfile = xmlData.Attributes.GetNamedItem("xdtfile").Value;
+                //var xdtfile = xmlData.Attributes.GetNamedItem("xdtfile").Value;
+                var fileEnd = "install.xdt";
+                if (uninstall)
+                {
+                    fileEnd = string.Format("un{0}", fileEnd);
+                }
+
+                var xdtfile = string.Format("{0}.{1}", xmlData.Attributes.GetNamedItem("xdtfile").Value, fileEnd);
                 string xdtFileName = VirtualPathUtility.ToAbsolute(xdtfile);
-                
+
                 // The translation at-hand
                 using (var xmlDoc = new XmlTransformableDocument())
                 {
@@ -47,20 +52,24 @@ namespace PageNotFoundManager.Umbraco.Installer
                         }
                     }
                 }
-
                 return true;
+            }
+
+            public bool Execute(string packageName, System.Xml.XmlNode xmlData)
+            {
+                return Transform(packageName, xmlData);
             }
 
             public System.Xml.XmlNode SampleXml()
             {
-                string str = "<Action runat=\"install\" undo=\"false\" alias=\"PNFM.TransformConfig\" file=\"~/web.config\" xdtfile=\"~/app_plugins/demo/web.config.xdt\">" +
+                string str = "<Action runat=\"install\" undo=\"true\" alias=\"PNFM.TransformConfig\" file=\"~/web.config\" xdtfile=\"~/app_plugins/demo/web.config\">" +
                          "</Action>";
                 return helper.parseStringToXmlNode(str);
             }
 
             public bool Undo(string packageName, System.Xml.XmlNode xmlData)
             {
-                return false;
+                return Transform(packageName, xmlData, true);
             }
         }
     }
