45eb2825c41255e25fbc796e380efe867b365eb6
Nik <nik.rimington@gmail.com>
2019-09-20 14:04:36 +0100

    Tweak to build script

--- BuildPackages/Package.build.v14.cmd
+++ BuildPackages/Package.build.v14.cmd
@@ -1,2 +1,2 @@
 nuget restore ../PageNotFoundManager.sln
-"C:\Program Files (x86)\MSBuild\15.0\Bin\MsBuild.exe" Package.build.xml -tv:Current /p:VisualStudioVersion=14.0
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" Package.build.xml -tv:Current /p:VisualStudioVersion=14.0
